1f6c1e66899e7e5295690c7217017c282186f86d
Jean-Christophe Filliatre <Jean-Christophe.Filliatre@cnrs.fr>
2026-08-21 08:36:19 +0200

    fixed and improved pretty-printer

--- spectec/targets/miniml/unparse.ml
+++ spectec/targets/miniml/unparse.ml
@@ -6,25 +6,34 @@ let flatten = Value.flatten_case_v
 
 let rec print_expr fmt e =
   match flatten e with
+  | _, ["Eapp"], [e1; e2] ->
+      fprintf fmt "@[%a@ %a@]" print_expr e1 print_atom e2
+  | _, ["Efun"], [x; e] ->
+      fprintf fmt "@[fun %s ->@ %a@]" (Value.get_text x)
+        print_expr e
+  | _, ["Elet"], [x; e1; e2] ->
+      fprintf fmt "@[let %s = %a in@ %a@]" (Value.get_text x)
+        print_expr e1 print_expr e2
+  | _ ->
+      print_atom fmt e
+
+and print_atom fmt e =
+  match flatten e with
   | _, ["Ecst"], [n] ->
       let n = Xl.Num.to_int (Value.get_num n) in
       fprintf fmt "%s" (Bigint.to_string n)
   | _, ["Eop"], [o] ->
       (match flatten o with
        | _, ["ADD"], _ -> fprintf fmt "+"
+       | _, ["SUB"], _ -> fprintf fmt "-"
+       | _, ["IFZ"], _ -> fprintf fmt "ifz"
        | _ -> failwith "unexpected operator")
   | _, ["Evar"], [x] ->
       fprintf fmt "%s" (Value.get_text x)
-  | _, ["Eapp"], [e1; e2] ->
-      fprintf fmt "(@[%a@ %a@])" print_expr e1 print_expr e2
-  | _, ["Efun"], [x; e] ->
-      fprintf fmt "(@[fun %s ->@ %a@])" (Value.get_text x)
-        print_expr e
-  | _, ["Elet"], [x; e1; e2] ->
-      fprintf fmt "(@[let %s = %a in@ %a@])" (Value.get_text x)
-        print_expr e1 print_expr e2
   | _, ["Epair"], [e1; e2] ->
       fprintf fmt "(@[%a,@ %a@])" print_expr e1 print_expr e2
+  | _, ["Eapp"|"Efun"|"Elet"], _ ->
+      fprintf fmt "(@[%a@])" print_expr e
   | _ ->
       fprintf fmt "<print_expr: unexpected term>"
 
